29fbb1ad08996057f0ee60d01d86a707270cdbdd
Loïc Mahieu <mahieuloic@gmail.com>
2016-01-14 12:11:16 +0100

    givenUser: Add possibility to pass `role` as object Add tests for givenUser etc...
    From https://github.com/strongloop/loopback-testing/pull/69

--- test/test.js
+++ test/test.js
@@ -6,6 +6,9 @@ describe('helpers', function () {
   var testApp = loopback();
   var db = testApp.dataSource('db', {connector: loopback.Memory});
   var testModel = testApp.model('xxx-test-model', {dataSource: 'db'});
+  testApp.model(loopback.User, {dataSource: 'db'});
+  testApp.model(loopback.Role, {dataSource: 'db'});
+  testApp.model(loopback.RoleMapping, {dataSource: 'db'});
 
   testApp.use(loopback.rest());
   helpers.beforeEach.withApp(testApp);
@@ -109,4 +112,50 @@ describe('helpers', function () {
       });
     });
   });
+
+ describe('givenUserWithRole', function() {
+   describe('role as string', function () {
+     helpers.beforeEach.givenUserWithRole(
+       {id: 1, email: "abc@abc.com", password: "abc"},
+       "testRole");
+     it("should create a user instance with default userModel with the given role", function() {
+       assert.equal(this['User'].id, 1);
+       assert.equal(this.userRole.id, 1);
+       assert.equal(this.userRole.name, "testRole");
+       assert(this.userRoleMapping);
+     });
+   })
+   describe('role as object', function () {
+     helpers.beforeEach.givenUserWithRole(
+       {id: 1, email: "abc@abc.com", password: "abc"},
+       {id: 2, name: "testRole"});
+     it("should create a user instance with default userModel with the given role", function() {
+       assert.equal(this['User'].id, 1);
+       assert.equal(this.userRole.id, 2);
+       assert.equal(this.userRole.name, "testRole");
+       assert(this.userRoleMapping);
+     });
+   })
+ });
+ describe('withUserModel', function() {
+   helpers.beforeEach.withUserModel('xxx-test-model');
+   it("should set the user model name", function() {
+     assert.equal(this.userModel, 'xxx-test-model');
+   });
+   describe('givenUser', function() {
+     helpers.beforeEach.givenUser();
+     it("should create a new instance of specified User model", function() {
+       assert(this[this.userModel]);
+     });
+   });
+   describe('givenUserWithRole', function() {
+     helpers.beforeEach.givenUserWithRole({id: 1}, {id: 2, name: "testRole"});
+     it("should create a user instance (of specified User model) with the given role", function() {
+       assert.equal(this[this.userModel].id, 1);
+       assert.equal(this.userRole.id, 2);
+       assert.equal(this.userRole.name, "testRole");
+       assert(this.userRoleMapping);
+     });
+   });
+ });
 });
