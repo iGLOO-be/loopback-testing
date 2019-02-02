56bca513cbba4bb6209deaa4868ed2743c1bca54
Loïc Mahieu <mahieuloic@gmail.com>
2019-02-02 11:52:49 +0100

    helpers: before

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var helpers = require('./lib/helpers');
 exports.describe = helpers.describe;
 exports.it = helpers.it;
 exports.beforeEach = helpers.beforeEach;
+exports.before = helpers.before;
 exports.TestDataBuilder = require('./lib/test-data-builder');
